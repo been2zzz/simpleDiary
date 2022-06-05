65b5ba7df5104d54a915823e36a33374e8195d71
been2zzz <been2zz@naver.com>
2022-06-05 23:36:13 +0900

    [UPDATE] DiaryItem component 분리

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,3 +1,5 @@
+import DiaryItem from "./DiaryItem";
+
 const DiaryList = ({ diaryList }) => {
   return (
     <div className="DiaryList">
@@ -5,13 +7,7 @@ const DiaryList = ({ diaryList }) => {
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((it) => (
-          <div key={it.id}>
-            {/* 최상위 태그에 key 할당 */}
-            <div>author {it.author}</div>
-            <div>Diary {it.content}</div>
-            <div>emotion {it.emotion}</div>
-            <div>create_at(ms) {it.created_date}</div>
-          </div>
+          <DiaryItem key={it.id} {...it} />
         ))}
       </div>
     </div>
