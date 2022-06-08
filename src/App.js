770a144ab089dfb5b775bffd5a924fb89d000358
been2zzz <been2zz@naver.com>
2022-06-08 14:23:46 +0900

    [ADD] useMemo를 이용한 일기 기분 분석

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-
-// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
@@ -13,6 +11,7 @@ function App() {
     const result = await fetch(
       'https://jsonplaceholder.typicode.com/comments'
     ).then((result) => result.json());
+
     const initData = result.slice(0, 20).map((it) => {
       return {
         author: it.email,
@@ -54,10 +53,25 @@ function App() {
     setData(newDiaryList);
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    // useMemo로 부터 값을 return 받기 때문에 더이상 함수가 아님
+    console.log('일기 분석 시작');
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]); // dependency array가 변화할 때만 실행 (data.length 즉, 일기 갯수가 변하지 않는 이상 실행 안함)
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className='App'>
       <h2>Diary</h2>
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
