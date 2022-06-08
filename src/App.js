a122dde404d0fae414c63952a7045e779dccd5d8
been2zzz <been2zz@naver.com>
2022-06-08 13:43:08 +0900

    [ADD] use json place holder API initData

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
 
+  const getData = async () => {
+    const result = await fetch(
+      'https://jsonplaceholder.typicode.com/comments'
+    ).then((result) => result.json());
+    const initData = result.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
